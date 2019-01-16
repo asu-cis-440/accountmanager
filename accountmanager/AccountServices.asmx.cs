cab36e316df4ac2ba05bad9c5a62e5d8335a103f
Jason <jrn8969@gmail.com>
2019-01-16 12:58:20 -0700

    This is the completed web services code

--- accountmanager/AccountServices.asmx.cs
+++ accountmanager/AccountServices.asmx.cs
@@ -24,20 +24,17 @@ namespace accountmanager
 	{
 
 		//EXAMPLE OF A SIMPLE SELECT QUERY (PARAMETERS PASSED IN FROM CLIENT)
-		[WebMethod]
+		[WebMethod(EnableSession = true)] //NOTICE: gotta enable session on each individual method
 		public bool LogOn(string uid, string pass)
 		{
-			//LOGIC: pass the parameters into the database to see if an account
-			//with these credentials exist.  If it does, then return true.  If
-			//it doesn't, then return false
-
 			//we return this flag to tell them if they logged in or not
 			bool success = false;
 
 			//our connection string comes from our web.config file like we talked about earlier
 			string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
 			//here's our query.  A basic select with nothing fancy.  Note the parameters that begin with @
-			string sqlSelect = "SELECT id FROM account WHERE userid=@idValue and pass=@passValue";
+			//NOTICE: we added admin to what we pull, so that we can store it along with the id in the session
+			string sqlSelect = "SELECT id, admin FROM account WHERE userid=@idValue and pass=@passValue";
 
 			//set up our connection object to be ready to use our connection string
 			MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
@@ -61,24 +58,29 @@ namespace accountmanager
 			//a legit account
 			if (sqlDt.Rows.Count > 0)
 			{
-				//flip our flag to true so we return a value that lets them know they're logged in
+				//if we found an account, store the id and admin status in the session
+				//so we can check those values later on other method calls to see if they 
+				//are 1) logged in at all, and 2) and admin or not
+				Session["id"] = sqlDt.Rows[0]["id"];
+				Session["admin"] = sqlDt.Rows[0]["admin"];
 				success = true;
 			}
 			//return the result!
 			return success;
 		}
 
-		[WebMethod]
-		public void LogOff()
+		[WebMethod(EnableSession = true)]
+		public bool LogOff()
 		{
-			//if they log off, we don't have much to do
-			//right now.  Later, we'll see how we can
-			//use this method to "forget" this user at
-			//the server level.
+			//if they log off, then we remove the session.  That way, if they access
+			//again later they have to log back on in order for their ID to be back
+			//in the session!
+			Session.Abandon();
+			return true;
 		}
 
 		//EXAMPLE OF AN INSERT QUERY WITH PARAMS PASSED IN.  BONUS GETTING THE INSERTED ID FROM THE DB!
-		[WebMethod]
+		[WebMethod(EnableSession = true)]
 		public void RequestAccount(string uid, string pass, string firstName, string lastName, string email)
 		{
 			string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
@@ -117,7 +119,7 @@ namespace accountmanager
 		}
 
 		//EXAMPLE OF A SELECT, AND RETURNING "COMPLEX" DATA TYPES
-		[WebMethod]
+		[WebMethod(EnableSession = true)]
 		public Account[] GetAccounts()
 		{
 			//check out the return type.  It's an array of Account objects.  You can look at our custom Account class in this solution to see that it's 
@@ -125,174 +127,221 @@ namespace accountmanager
 			//sets of information, it's a good idea to create a custom container class to represent instances (or rows) of that information, and then return an array of those objects.  
 			//Keeps everything simple.
 
-			//LOGIC: get all the active accounts and return them!
+			//WE ONLY SHARE ACCOUNTS WITH LOGGED IN USERS!
+			if (Session["id"] != null)
+			{
+				DataTable sqlDt = new DataTable("accounts");
 
-			DataTable sqlDt = new DataTable("accounts");
+				string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+				string sqlSelect = "select id, userid, pass, firstname, lastname, email from account where active=1 order by lastname";
 
-			string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
-			string sqlSelect = "select id, userid, pass, firstname, lastname, email from account where active=1 order by lastname";
-			
-			MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
-			MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+				MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+				MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
 
-			//gonna use this to fill a data table
-			MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
-			//filling the data table
-			sqlDa.Fill(sqlDt);
-		
-			//loop through each row in the dataset, creating instances
-			//of our container class Account.  Fill each acciount with
-			//data from the rows, then dump them in a list.
-			List<Account> accounts = new List<Account>();
-				for (int i = 0; i<sqlDt.Rows.Count; i++)
+				//gonna use this to fill a data table
+				MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+				//filling the data table
+				sqlDa.Fill(sqlDt);
+
+				//loop through each row in the dataset, creating instances
+				//of our container class Account.  Fill each acciount with
+				//data from the rows, then dump them in a list.
+				List<Account> accounts = new List<Account>();
+				for (int i = 0; i < sqlDt.Rows.Count; i++)
 				{
-					accounts.Add(new Account
+					//only share user id and pass info with admins!
+					if (Convert.ToInt32(Session["admin"]) == 1)
+					{
+						accounts.Add(new Account
+						{
+							id = Convert.ToInt32(sqlDt.Rows[i]["id"]),
+							userId = sqlDt.Rows[i]["userid"].ToString(),
+							password = sqlDt.Rows[i]["pass"].ToString(),
+							firstName = sqlDt.Rows[i]["firstname"].ToString(),
+							lastName = sqlDt.Rows[i]["lastname"].ToString(),
+							email = sqlDt.Rows[i]["email"].ToString()
+						});
+					}
+					else
+					{
+						accounts.Add(new Account
+						{
+							id = Convert.ToInt32(sqlDt.Rows[i]["id"]),
+							firstName = sqlDt.Rows[i]["firstname"].ToString(),
+							lastName = sqlDt.Rows[i]["lastname"].ToString(),
+							email = sqlDt.Rows[i]["email"].ToString()
+						});
+					}
+				}
+				//convert the list of accounts to an array and return!
+				return accounts.ToArray();
+			}
+			else
+			{
+				//if they're not logged in, return an empty array
+				return new Account[0];
+			}
+		}
+
+		//EXAMPLE OF AN UPDATE QUERY WITH PARAMS PASSED IN
+		[WebMethod(EnableSession = true)]
+		public void UpdateAccount(string id, string uid, string pass, string firstName, string lastName, string email)
+		{
+			//WRAPPING THE WHOLE THING IN AN IF STATEMENT TO CHECK IF THEY ARE AN ADMIN!
+			if (Convert.ToInt32(Session["admin"]) == 1)
+			{
+				string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+				//this is a simple update, with parameters to pass in values
+				string sqlSelect = "update account set userid=@uidValue, pass=@passValue, firstname=@fnameValue, lastname=@lnameValue, " +
+					"email=@emailValue where id=@idValue";
+
+				MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+				MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+				sqlCommand.Parameters.AddWithValue("@uidValue", HttpUtility.UrlDecode(uid));
+				sqlCommand.Parameters.AddWithValue("@passValue", HttpUtility.UrlDecode(pass));
+				sqlCommand.Parameters.AddWithValue("@fnameValue", HttpUtility.UrlDecode(firstName));
+				sqlCommand.Parameters.AddWithValue("@lnameValue", HttpUtility.UrlDecode(lastName));
+				sqlCommand.Parameters.AddWithValue("@emailValue", HttpUtility.UrlDecode(email));
+				sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(id));
+
+				sqlConnection.Open();
+				//we're using a try/catch so that if the query errors out we can handle it gracefully
+				//by closing the connection and moving on
+				try
+				{
+					sqlCommand.ExecuteNonQuery();
+				}
+				catch (Exception e)
+				{
+				}
+				sqlConnection.Close();
+			}
+		}
+
+
+
+
+
+
+
+
+
+		//EXAMPLE OF A SELECT, AND RETURNING "COMPLEX" DATA TYPES
+		[WebMethod(EnableSession = true)]
+		public Account[] GetAccountRequests()
+		{//LOGIC: get all account requests and return them!
+			if (Convert.ToInt32(Session["admin"]) == 1)
+			{
+				DataTable sqlDt = new DataTable("accountrequests");
+
+				string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+				//requests just have active set to 0
+				string sqlSelect = "select id, userid, pass, firstname, lastname, email from account where active=0 order by lastname";
+
+				MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+				MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+				MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+				sqlDa.Fill(sqlDt);
+
+				List<Account> accountRequests = new List<Account>();
+				for (int i = 0; i < sqlDt.Rows.Count; i++)
+				{
+					accountRequests.Add(new Account
 					{
 						id = Convert.ToInt32(sqlDt.Rows[i]["id"]),
-						userId = sqlDt.Rows[i]["userid"].ToString(),
-						password = sqlDt.Rows[i]["pass"].ToString(),
 						firstName = sqlDt.Rows[i]["firstname"].ToString(),
 						lastName = sqlDt.Rows[i]["lastname"].ToString(),
 						email = sqlDt.Rows[i]["email"].ToString()
 					});
 				}
 				//convert the list of accounts to an array and return!
-				return accounts.ToArray();
-		}
-
-		//EXAMPLE OF AN UPDATE QUERY WITH PARAMS PASSED IN
-		[WebMethod]
-		public void UpdateAccount(string id, string uid, string pass, string firstName, string lastName, string email)
-		{
-			string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
-			//this is a simple update, with parameters to pass in values
-			string sqlSelect = "update account set userid=@uidValue, pass=@passValue, firstname=@fnameValue, lastname=@lnameValue, " +
-				"email=@emailValue where id=@idValue";
-
-			MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
-			MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
-
-			sqlCommand.Parameters.AddWithValue("@uidValue", HttpUtility.UrlDecode(uid));
-			sqlCommand.Parameters.AddWithValue("@passValue", HttpUtility.UrlDecode(pass));
-			sqlCommand.Parameters.AddWithValue("@fnameValue", HttpUtility.UrlDecode(firstName));
-			sqlCommand.Parameters.AddWithValue("@lnameValue", HttpUtility.UrlDecode(lastName));
-			sqlCommand.Parameters.AddWithValue("@emailValue", HttpUtility.UrlDecode(email));
-			sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(id));
-
-			sqlConnection.Open();
-			//we're using a try/catch so that if the query errors out we can handle it gracefully
-			//by closing the connection and moving on
-			try
-			{
-				sqlCommand.ExecuteNonQuery();
+				return accountRequests.ToArray();
 			}
-			catch (Exception e)
-			{
+			else {
+				return new Account[0];
 			}
-			sqlConnection.Close();
-		}
-
-		//EXAMPLE OF A SELECT, AND RETURNING "COMPLEX" DATA TYPES
-		[WebMethod]
-		public Account[] GetAccountRequests()
-		{//LOGIC: get all account requests and return them!
-
-			DataTable sqlDt = new DataTable("accountrequests");
-
-			string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
-			//requests just have active set to 0
-			string sqlSelect = "select id, userid, pass, firstname, lastname, email from account where active=0 order by lastname";
-
-			MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
-			MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
-
-			MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
-			sqlDa.Fill(sqlDt);
-			
-			List<Account> accountRequests = new List<Account>();
-			for (int i = 0; i < sqlDt.Rows.Count; i++)
-			{
-				accountRequests.Add(new Account
-				{
-					id = Convert.ToInt32(sqlDt.Rows[i]["id"]),
-					firstName = sqlDt.Rows[i]["firstname"].ToString(),
-					lastName = sqlDt.Rows[i]["lastname"].ToString(),
-					email = sqlDt.Rows[i]["email"].ToString()
-				});
-			}
-			//convert the list of accounts to an array and return!
-			return accountRequests.ToArray();
 		}
 
 		//EXAMPLE OF A DELETE QUERY
-		[WebMethod]
+		[WebMethod(EnableSession = true)]
 		public void DeleteAccount(string id)
 		{
-			string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
-			//this is a simple update, with parameters to pass in values
-			string sqlSelect = "delete from account where id=@idValue";
-
-			MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
-			MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
-			
-			sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(id));
-
-			sqlConnection.Open();
-			try
+			if (Convert.ToInt32(Session["admin"]) == 1)
 			{
-				sqlCommand.ExecuteNonQuery();
+				string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+				//this is a simple update, with parameters to pass in values
+				string sqlSelect = "delete from account where id=@idValue";
+
+				MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+				MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+				sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(id));
+
+				sqlConnection.Open();
+				try
+				{
+					sqlCommand.ExecuteNonQuery();
+				}
+				catch (Exception e)
+				{
+				}
+				sqlConnection.Close();
 			}
-			catch (Exception e)
-			{
-			}
-			sqlConnection.Close();
 		}
 
 		//EXAMPLE OF AN UPDATE QUERY
-		[WebMethod]
+		[WebMethod(EnableSession = true)]
 		public void ActivateAccount(string id)
 		{
-			string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
-			//this is a simple update, with parameters to pass in values
-			string sqlSelect = "update account set active=1 where id=@idValue";
-
-			MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
-			MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
-
-			sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(id));
-
-			sqlConnection.Open();
-			try
+			if (Convert.ToInt32(Session["admin"]) == 1)
 			{
-				sqlCommand.ExecuteNonQuery();
+				string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+				//this is a simple update, with parameters to pass in values
+				string sqlSelect = "update account set active=1 where id=@idValue";
+
+				MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+				MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+				sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(id));
+
+				sqlConnection.Open();
+				try
+				{
+					sqlCommand.ExecuteNonQuery();
+				}
+				catch (Exception e)
+				{
+				}
+				sqlConnection.Close();
 			}
-			catch (Exception e)
-			{
-			}
-			sqlConnection.Close();
 		}
 
-		//EXAMPLE OF AN UPDATE QUERY
-		[WebMethod]
+		//EXAMPLE OF A DELETE QUERY
+		[WebMethod(EnableSession = true)]
 		public void RejectAccount(string id)
 		{
-			string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
-			string sqlSelect = "delete from account where id=@idValue";
-
-			MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
-			MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
-
-			sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(id));
-
-			sqlConnection.Open();
-			try
+			if (Convert.ToInt32(Session["admin"]) == 1)
 			{
-				sqlCommand.ExecuteNonQuery();
+				string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+				string sqlSelect = "delete from account where id=@idValue";
+
+				MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+				MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+				sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(id));
+
+				sqlConnection.Open();
+				try
+				{
+					sqlCommand.ExecuteNonQuery();
+				}
+				catch (Exception e)
+				{
+				}
+				sqlConnection.Close();
 			}
-			catch (Exception e)
-			{
-			}
-			sqlConnection.Close();
 		}
 
 
