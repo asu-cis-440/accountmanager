ae4d778d0b8d58fe6f9ab99065c74c062a592419
asu-cis-440 <jrn8969@gmail.com>
2019-10-31 09:55:26 -0700

    added file upload example

---
+++ accountmanager/uploadbutton.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="uploadbutton.aspx.cs" Inherits="accountmanager.uploadbutton" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+		<fieldset>
+			<legend>
+				...or hey upload a file!
+			</legend>
+    	<asp:FileUpload ID="Uploader" runat="server" />
+    	<asp:Button ID="UploadSubmit" runat="server" OnClick="UploadSubmit_Click" Text="Upload" Height="40px" BackColor="White" BorderStyle="None" ForeColor="Blue" />
+		</fieldset>
+    <asp:Label ID="StatusLabel" runat="server" Font-Italic="True"></asp:Label>
+		<br />
+    </form>
+	
+</body>
+</html>
---
+++ accountmanager/uploadbutton.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace accountmanager
+{
+	public partial class uploadbutton : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void UploadSubmit_Click(object sender, EventArgs e)
+		{
+			if (Uploader.HasFile)
+			{
+				try
+				{
+					string filename = Path.GetFileName(Uploader.FileName);
+					Uploader.SaveAs(Server.MapPath("~/") + "uploads/" + filename);
+					string[] textLines = File.ReadAllLines(Server.MapPath("~/")+"/uploads/" + filename);
+
+
+					StatusLabel.Text = "Success! First line of text: "+textLines[0];
+
+
+				}
+				catch (Exception ex)
+				{
+					StatusLabel.Text = "Error: " + ex.Message;
+				}
+			}
+		}
+	}
+}
