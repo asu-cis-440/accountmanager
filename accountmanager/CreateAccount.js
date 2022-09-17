164266e1d534f5aa08bcf518b22c8ac94a81c1e7
maleneez <Maleneez@asu.edu>
2022-09-17 13:28:22 +0300

    Worked on Account Creation

---
+++ accountmanager/CreateAccount.js
@@ -0,0 +1,40 @@
+﻿
+
+function openDialog() {
+   
+}
+
+
+
+function createAccount(addPet) {
+
+    if (document.getElementById("username").value != '' && document.getElementById("password").value != '') {
+        var isConfirmed = confirm("Do you want to create an account?");
+    }
+
+    if (isConfirmed == true) {
+        username = document.getElementById("username").value;
+        password = document.getElementById("password").value;
+        photo = "test.jpg";
+        var mtd = "AccountServices.asmx/CreateAccount";
+  
+        $.ajax({
+            type: "POST",
+            url: mtd,
+            data: JSON.stringify({ "username": username, "password": password, "photo": photo }),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+        }, function (data, status) {
+            alert("Data: " + data + "\nStatus: " + status);
+        });
+        clear();
+    } else {
+        clear();
+    }
+}
+
+
+function clear() {
+    document.getElementById("username").value = "";
+    document.getElementById("password").innerHTML = "";
+}
